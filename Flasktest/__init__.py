dc4312cf34700a48fd4464f510aea01e6088b94e
Dan Hilton <daniel.hilton@gmail.com>
2015-06-17 14:57:16 +0100

    Add Flask-Stats to enable application level metrics
    To enable application metrics, we have included the application library
    Flask-stats, which allows us to log a series of metrics about our
    application to a `Statsd` host.

--- Flasktest/__init__.py
+++ Flasktest/__init__.py
@@ -2,6 +2,9 @@ from __future__ import with_statement
 from contextlib import closing
 from Flasktest.database import db_session
 from flask import Flask
+from flask.ext.stats import Stats
+
+import Flasktest.views
 
 #configuration
 DATABASE = '/tmp/flasktest.db'
@@ -14,7 +17,17 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-import Flasktest.views
+
+# Flask stats configuration
+# STATS_HOSTNAME
+app.config.from_envvar('STATS_HOSTNAME')
+# STATS_PORT
+app.config.from_envvar('STATS_PORT')
+# STATS_BASE_KEY
+app.config.from_envvar('STATS_BASE_KEY')
+
+# Initialise flask-metrics
+stats = Stats().init_app(app)
 
 @app.teardown_request
 def teardown_request(exception):
