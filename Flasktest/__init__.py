cd898db4021256a42418255da345f1a10087277c
Dan Hilton <daniel.hilton@gmail.com>
2015-06-19 08:01:25 +0100

    Update to fix import error
    Due to a testing error in depoyment on tsuru, namely that when you push to a Gandalf git repo without
    specificly stating which local branch you want pushed, you push master, a cyclical import error crept in.
    
    This commit reverts the import issue (caused by a cleanup change) and fixes the use of environment variables
    for the metrics service. Mitigation of issues like this in the future would be to have a lightweight set
    of local tests.

--- Flasktest/__init__.py
+++ Flasktest/__init__.py
@@ -3,10 +3,9 @@ from contextlib import closing
 from Flasktest.database import db_session
 from flask import Flask
 from flask.ext.stats import Stats
+import os
 
-import Flasktest.views
-
-#configuration
+# configuration
 DATABASE = '/tmp/flasktest.db'
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -17,19 +16,22 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+import Flasktest.views
+
 
 # Flask stats configuration
 # STATS_HOSTNAME
-app.config.from_envvar('STATS_HOSTNAME')
+app.config['STATS_HOSTNAME'] = os.environ.get('STATS_HOSTNAME', "")
 # STATS_PORT
-app.config.from_envvar('STATS_PORT')
+app.config['STATS_PORT'] = os.environ.get('STATS_PORT', "")
 # STATS_BASE_KEY
-app.config.from_envvar('STATS_BASE_KEY')
+app.config['STATS_BASE_KEY'] = os.environ.get('STATS_BASE_KEY', "")
 
-# Initialise flask-metrics
-stats = Stats().init_app(app)
+# Initialise flask-metrics, only if connection setup
+if app.config['STATS_HOSTNAME'] != "":
+	stats = Stats().init_app(app)
+
 
 @app.teardown_request
 def teardown_request(exception):
-  db_session.remove()
-
+    db_session.remove()
